de056849311476a1613a35e6e3fbfd55f269ca3e
Néstor <Nstor@Nstor>
2016-10-27 11:01:39 +0100

    Kata 4:version2

--- src/kata4/Kata4.java
+++ src/kata4/Kata4.java
@@ -6,10 +6,17 @@ import java.io.File;
 public class Kata4 {
 
     public static void main(String[] args) {
-        File file=new File("c:\\");
-        String[] names=file.list();
-        for(String name:names){
-            System.out.println(name);
+        File file=new File("C:\\Users\\Nes\\Desktop\\Clase\\Prueba");
+        print(file.listFiles(),"");
+    }
+    
+    public static void print(File[] files, String indent){
+        if(files==null) return;
+        for(File file : files){
+            System.out.println(indent + (file.isDirectory()? 
+                    "+": "-")+ file.getName());
+            if(!file.isDirectory()|| file.isHidden()) continue;
+            print(file.listFiles(), " ");
         }
     }
     
